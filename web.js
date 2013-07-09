3d7e17f9df097be6f387180e54cdaa68161b115c
Andrew Chung <archung@gmail.com>
2013-07-09 10:29:38 +0000

    read from index.html instead

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
+var index = fs.readFileSync("index.html");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+ response.send(index.toString());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+ 
