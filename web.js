2363a59dcdfcf04b01583649af5c438ecec5b5e9
Firekat <firekat@chungy.org>
2013-08-05 21:50:46 +0000

    Added the web.js version that support live refreshing stuff

--- web.js
+++ web.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-
+var htmlfile = "index.html";
 var index = fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
  response.send(index.toString());
 });
 
-var port = process.env.PORT || 5000;
+app.get("/blah", function(request, response) {
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
+});
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
